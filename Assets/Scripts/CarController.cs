94b04e40d36d7d731aed5c876064a95741966e9b
CodeNerd20 <EthanSchlosser19@gmail.com>
2024-01-03 10:23:34 -0800

    1/3/2023

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,9 +6,16 @@ public class CarController : MonoBehaviour
 {
     public float speed = 5f;
     public float turnSpeed = 90f;
+
     public Rigidbody rb;
+
     public float horizontalInput;
     public float forwardInput;
+
+    public GameObject bullet;
+    public Rigidbody bulletRb;
+    public float spawnRate;
+    public float bulletSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +30,12 @@ public class CarController : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bullet, transform.position, bullet.transform.rotation);
+            bulletRb.AddForce(Vector3.forward * bulletSpeed * Time.deltaTime, ForceMode.Impulse);
+        }
     }
 }
